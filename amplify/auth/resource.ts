faf2af8ed7dc3a0339e9e5c7ec65c234bfca2323
Minh Dang Le <minhdang2001.11@gmail.com>
2024-10-29 10:39:31 +0700

    add dependencies

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -12,7 +12,7 @@ import { verifyAuthChallengeResponse } from "./verify-auth-challenge-response/re
 
 
 export const auth = defineAuth({
-  name: "dotebaby_user_pool",
+  name: "DoteBaby",
   loginWith: {
     
    email: {
