b27a1949d828022b5bcd1f3403e2769f48cd1cef
Minh Dang Le <minhdang2001.11@gmail.com>
2024-10-30 11:28:01 +0700

    debugging

--- amplify/auth/create-auth-challenge/handler.ts
+++ amplify/auth/create-auth-challenge/handler.ts
@@ -1,63 +1,25 @@
 import type { CreateAuthChallengeTriggerHandler } from "aws-lambda";
 
 export const handler: CreateAuthChallengeTriggerHandler = async (event) => {
-  // if (event.request.challengeName === "CUSTOM_CHALLENGE") {
+  if (event.request.challengeName === "CUSTOM_CHALLENGE") {
     
-  //   const digitGenerator = require('crypto-secure-random-digit');
+    const digitGenerator = require('crypto-secure-random-digit');
     
-  //   let challengeCode = digitGenerator.randomDigits(6).join("");
-  //   console.log(`Create: go here: ${challengeCode}`)
-  //   await sendChallengeCode(event.request.userAttributes.email, challengeCode);
+    let challengeCode = digitGenerator.randomDigits(6).join("");
+    console.log(`Create: go here: ${challengeCode}`)
+    await sendChallengeCode(event.request.userAttributes.email, challengeCode);
 
-  //   event.response.challengeMetadata = "TOKEN_CHECK";
+    event.response.challengeMetadata = "TOKEN_CHECK";
 
-  //   event.response.publicChallengeParameters = {
-  //     trigger: "true",
-  //     code: challengeCode,
-  //   };
-  //   event.response.privateChallengeParameters = { trigger: "true" };
-  //   event.response.privateChallengeParameters.answer = challengeCode;
-  // }
-  
-  // return event;
-
-
-
-    let secretLoginCode: string;
-    if (!event.request.session || !event.request.session.length) {
-      console.log("Create case 1:")
-        // This is a new auth session
-      // Generate a new secret login code and mail it to the user
-        const digitGenerator = require('crypto-secure-random-digit');
-        secretLoginCode = digitGenerator.randomDigits(6).join("");
-        await sendChallengeCode(event.request.userAttributes.email, secretLoginCode);
-
-    } else {
-
-        // There's an existing session. Don't generate new digits but
-        // re-use the code from the current session. This allows the user to
-        // make a mistake when keying in the code and to then retry, rather
-        // the needing to e-mail the user an all new code again.    
-        const previousChallenge = event.request.session.slice(-1)[0];
-      secretLoginCode = previousChallenge.challengeMetadata!.match(/CODE-(\d*)/)![1];
-      console.log(`Create case 2: secret code ${secretLoginCode}`)
-    }
-
-    // This is sent back to the client app
     event.response.publicChallengeParameters = {
-        email: event.request.userAttributes.email
+      trigger: "true",
+      code: challengeCode,
     };
-
-    // Add the secret login code to the private challenge parameters
-    // so it can be verified by the "Verify Auth Challenge Response" trigger
-    event.response.privateChallengeParameters = { secretLoginCode };
-
-    // Add the secret login code to the session so it is available
-    // in a next invocation of the "Create Auth Challenge" trigger
-  event.response.challengeMetadata = `CODE-${secretLoginCode}`;
-  console.log(`Create response ${event.response.challengeMetadata}`)
-
-    return event;
+    event.response.privateChallengeParameters = { trigger: "true" };
+    event.response.privateChallengeParameters.answer = challengeCode;
+  }
+  
+  return event;
 };
 
 async function sendChallengeCode(emailAddress: string, secretCode: string) {
--- amplify/auth/define-auth-challenge/handler.ts
+++ amplify/auth/define-auth-challenge/handler.ts
@@ -1,60 +1,29 @@
 import type { DefineAuthChallengeTriggerHandler } from "aws-lambda"
 
 export const handler: DefineAuthChallengeTriggerHandler = async (event) => {
-  // // Check if this is the first authentication attempt
-  //   if (event.request.session.length === 0) {
-  //   // For the first attempt, we start with the custom challenge
-  //   event.response.issueTokens = false;
-  //   event.response.failAuthentication = false;
-  //   event.response.challengeName = "CUSTOM_CHALLENGE";
-  // } else if (
-  //   event.request.session.length === 1 &&
-  //   event.request.session[0].challengeName === "CUSTOM_CHALLENGE" &&
-  //   event.request.session[0].challengeResult === true
-  //   ) {
-  //   // If this is the second attempt (session length 1),
-  //   // it was a CUSTOM_CHALLENGE, and the result was successful
-  //   event.response.issueTokens = true;
-  //   event.response.failAuthentication = false;
-  //   } else {
-  //   // If we reach here, it means either:
-  //   // 1. The custom challenge failed
-  //   // 2. We've gone through more attempts than expected
-  //   // In either case, we fail the authentication
-  //   event.response.issueTokens = false;
-  //   event.response.failAuthentication = true;
-  // }
-
-  if (event.request.session &&
-    event.request.session.find(attempt => attempt.challengeName !== 'CUSTOM_CHALLENGE')) {
-        console.log("Define case 1:")
-        // We only accept custom challenges; fail auth
-        event.response.issueTokens = false;
-        event.response.failAuthentication = true;
-    } else if (event.request.session &&
-        event.request.session.length >= 3 &&
-    event.request.session.slice(-1)[0].challengeResult === false) {
-        console.log("Define case 2:")
-        // The user provided a wrong answer 3 times; fail auth
-        event.response.issueTokens = false;
-        event.response.failAuthentication = true;
-    } else if (event.request.session &&
-        event.request.session.length &&
-        event.request.session.slice(-1)[0].challengeName === 'CUSTOM_CHALLENGE' && // Doubly stitched, holds better
-    event.request.session.slice(-1)[0].challengeResult === true) {
-        console.log("Define case 3:")
-        // The user provided the right answer; succeed auth
-        event.response.issueTokens = true;
-        event.response.failAuthentication = false;
+  // Check if this is the first authentication attempt
+    if (event.request.session.length === 0) {
+    // For the first attempt, we start with the custom challenge
+    event.response.issueTokens = false;
+    event.response.failAuthentication = false;
+    event.response.challengeName = "CUSTOM_CHALLENGE";
+  } else if (
+    event.request.session.length === 1 &&
+    event.request.session[0].challengeName === "CUSTOM_CHALLENGE" &&
+    event.request.session[0].challengeResult === true
+    ) {
+    // If this is the second attempt (session length 1),
+    // it was a CUSTOM_CHALLENGE, and the result was successful
+    event.response.issueTokens = true;
+    event.response.failAuthentication = false;
     } else {
-    // The user did not provide a correct answer yet; present challenge
-        console.log("Define case 4:")
-        event.response.issueTokens = false;
-        event.response.failAuthentication = false;
-        event.response.challengeName = 'CUSTOM_CHALLENGE';
-    }
-
-    return event;
+    // If we reach here, it means either:
+    // 1. The custom challenge failed
+    // 2. We've gone through more attempts than expected
+    // In either case, we fail the authentication
+    event.response.issueTokens = false;
+    event.response.failAuthentication = true;
+  }
 
   return event;
 };
