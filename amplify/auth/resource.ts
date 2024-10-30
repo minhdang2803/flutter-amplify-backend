ab16ac44359998f187a13899592d91d0165132f5
Minh Dang Le <minhdang2001.11@gmail.com>
2024-10-30 13:38:35 +0700

    debugging

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,26 +7,20 @@ import { preSignUpTrigger } from './pre-sign-up-trigger/resource';
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-
-
-
-
-
 export const auth = defineAuth({
   name: "DoteBaby",
   loginWith: {
-    
-   email: {
-     // can be used in conjunction with a customized welcome email as well
-     verificationEmailStyle: "CODE",
-     verificationEmailSubject: "Welcome to my app!",
-     verificationEmailBody: (createCode) => `Use this code to confirm your account: ${createCode()}`,
-     userInvitation: {
-       emailSubject: "Welcome to my app!",
-       emailBody: (user, code) =>
-         `We're happy to have you! You can now login with username ${user()} and temporary password ${code()}`, 
-     },
-   },
+    email: {
+      // can be used in conjunction with a customized welcome email as well
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to my app!",
+      verificationEmailBody: (createCode) => `Use this code to confirm your account: ${createCode()}`,
+      userInvitation: {
+        emailSubject: "Welcome to my app!",
+        emailBody: (user, code) =>
+          `We're happy to have you! You can now login with username ${user()} and temporary password ${code()}`, 
+      },
+    },
         externalProviders: {
         google: {
           clientId: secret('googleClientId'),
