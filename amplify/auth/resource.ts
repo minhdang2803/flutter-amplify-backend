007eb0648f79033d1b9b736e5ece4b593bc9e8c6
Minh Dang Le <minhdang2001.11@gmail.com>
2024-11-05 16:19:21 +0700

    fix again mapping attributes

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,6 +3,7 @@ import { createAuthChallenge } from "./create-auth-challenge/resource"
 import { defineAuthChallenge } from "./define-auth-challenge/resource"
 import { verifyAuthChallengeResponse } from "./verify-auth-challenge-response/resource"
 import { preSignUpTrigger } from './pre-sign-up-trigger/resource';
+import { IUserPool } from 'aws-cdk-lib/aws-cognito';
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
@@ -10,17 +11,7 @@ import { preSignUpTrigger } from './pre-sign-up-trigger/resource';
 export const auth = defineAuth({
   name: "DoteBaby",
   loginWith: {
-    email: {
-      // can be used in conjunction with a customized welcome email as well
-      verificationEmailStyle: "CODE",
-      verificationEmailSubject: "Welcome to my app!",
-      verificationEmailBody: (createCode) => `Use this code to confirm your account: ${createCode()}`,
-      userInvitation: {
-        emailSubject: "Welcome to my app!",
-        emailBody: (user, code) =>
-          `We're happy to have you! You can now login with username ${user()} and temporary password ${code()}`, 
-      },
-    },
+    email: true,
     externalProviders: {
       google: {
         clientId: secret('googleClientIdDemo'),
@@ -33,8 +24,7 @@ export const auth = defineAuth({
         teamId: secret('appleTeamIdDemo')
       },
       callbackUrls: ["dotebaby://medotebaby.com/"],
-    logoutUrls: ["dotebaby://medotebaby.com/"],
-    // logoutUrls: ["dotebaby://meDoteBaby.com/"],
+      logoutUrls: ["dotebaby://medotebaby.com/"],
     }
   },
   triggers: {
